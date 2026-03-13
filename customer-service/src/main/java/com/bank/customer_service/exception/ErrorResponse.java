18f0fa00668c0ee54421344192e3ab77d57bde65
KamilKowalczyk8 <kamil.kowalczyk9.1@onet.pl>
2026-03-13 18:50:32 +0100

    Dodanie obsługi błędów exception

---
+++ customer-service/src/main/java/com/bank/customer_service/exception/ErrorResponse.java
@@ -0,0 +1,10 @@
+package com.bank.customer_service.exception;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(
+        LocalDateTime timestamp,
+        int status,
+        String error,
+        String message
+) {}
