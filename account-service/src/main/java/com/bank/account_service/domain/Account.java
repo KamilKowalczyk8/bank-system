64b3ed38c6ad1f3f926dd9297f77d156d94e9862
KamilKowalczyk8 <kamil.kowalczyk9.1@onet.pl>
2026-03-19 20:38:35 +0100

    feat: implementacja AccountNumber (Value Object) oraz algorytmu Modulo 97
    - account-service: utworzenie klasy AccountNumber dla pełnej hermetyzacji numeru rachunku bankowego w domenie
    
    - account-service: dodanie twardej walidacji sumy kontrolnej polskiego standardu NRB/IBAN (Modulo 97)
    
    - account-service: wdrożenie metody fabrykującej generateNew(sequenceNumber) dedykowanej dla nowych klientów
    
    - account-service: nadpisanie metod equals() i hashCode() w celu poprawnego porównywania obiektów po ich wartości
    
    - account-service: refaktoryzacja głównej klasy Account - zastąpienie typu String bezpiecznym Value Objectem

--- account-service/src/main/java/com/bank/account_service/domain/Account.java
+++ account-service/src/main/java/com/bank/account_service/domain/Account.java
@@ -2,30 +2,27 @@ package com.bank.account_service.domain;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.Objects;
 import java.util.UUID;
 
 public class Account {
 
     private final UUID accountId;
     private final String customerId;
-    private final String accountNumber;
+    private final AccountNumber accountNumber;
     private BigDecimal balance;
     private final Currency currency;
     private AccountStatus status;
     private final Instant createdAt;
 
-    public Account(String customerId, String accountNumber, Currency currency) {
+    public Account(String customerId, AccountNumber accountNumber, Currency currency) {
         if (customerId == null || customerId.isBlank()) {
             throw new IllegalArgumentException("ID klienta nie może być puste.");
-        }
-        if (accountNumber == null || accountNumber.length() != 26) {
-            throw new IllegalArgumentException("Nieprawidłowy numer konta. Wymagane 26 cyfr.");
         }
 
         this.accountId = UUID.randomUUID();
         this.customerId = customerId;
-        this.accountNumber = accountNumber;
-        this.balance = BigDecimal.ZERO;
+        this.accountNumber = Objects.requireNonNull(accountNumber, "Numer konta nie może być null");        this.balance = BigDecimal.ZERO;
         this.currency = currency != null ? currency : Currency.PLN;
         this.status = AccountStatus.ACTIVE;
         this.createdAt = Instant.now();
@@ -66,7 +63,7 @@ public class Account {
         return accountId;
     }
     public String getCustomerId() { return customerId; }
-    public String getAccountNumber() { return accountNumber; }
+    public AccountNumber getAccountNumber() { return accountNumber; }
     public BigDecimal getBalance() { return balance; }
     public Currency getCurrency() { return currency; }
     public AccountStatus getStatus() { return status; }
