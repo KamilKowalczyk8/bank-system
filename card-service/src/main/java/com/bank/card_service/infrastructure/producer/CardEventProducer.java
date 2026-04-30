27698da443d13ad82df45aa43f4ead84dba2c8cf
KamilKowalczyk8 <kamil.kowalczyk9.1@onet.pl>
2026-04-30 22:41:30 +0200

    poprawka w card-service cardeventproducer 2

--- card-service/src/main/java/com/bank/card_service/infrastructure/producer/CardEventProducer.java
+++ card-service/src/main/java/com/bank/card_service/infrastructure/producer/CardEventProducer.java
@@ -3,8 +3,6 @@ package com.bank.card_service.infrastructure.producer;
 import com.bank.card_service.application.port.out.CardEventPublisher;
 import com.bank.card_service.infrastructure.dto.event.CardCreatedEvent;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
