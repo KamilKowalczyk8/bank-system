5458954884a3025afaf7d25193ece270e6678af4
KamilKowalczyk8 <kamil.kowalczyk9.1@onet.pl>
2026-05-28 18:49:11 +0200

    dodanie componentscan do document-service

--- document-service/src/main/java/com/bank/document_service/DocumentServiceApplication.java
+++ document-service/src/main/java/com/bank/document_service/DocumentServiceApplication.java
@@ -2,8 +2,11 @@ package com.bank.document_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.bank.document_service", "com.bank.common"})
+
 public class DocumentServiceApplication {
 
 	public static void main(String[] args) {
