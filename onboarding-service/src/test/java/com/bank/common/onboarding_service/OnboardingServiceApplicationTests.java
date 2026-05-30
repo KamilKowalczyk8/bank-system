06a857ce5a22323a0b15209c2fce994f26e5707c
KamilKowalczyk8 <kamil.kowalczyk9.1@onet.pl>
2026-05-30 12:52:20 +0200

    fix fast

--- onboarding-service/src/test/java/com/bank/common/onboarding_service/OnboardingServiceApplicationTests.java
+++ onboarding-service/src/test/java/com/bank/common/onboarding_service/OnboardingServiceApplicationTests.java
@@ -10,12 +10,7 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest(
-		classes = OnboardingServiceApplication.class,
-		properties = {
-				"spring.mvc.pathmatch.matching-strategy=ant_path_matcher"
-		}
-)
+@SpringBootTest(classes = OnboardingServiceApplication.class)
 @EnableAutoConfiguration(exclude = {
 		DataSourceAutoConfiguration.class,
 		DataSourceTransactionManagerAutoConfiguration.class,
